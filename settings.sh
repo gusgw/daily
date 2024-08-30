3e194e06e7bc98d3f7e0a51e7ad51681195562c9
Angus Gray-Weale <gusgw@gusgw.net>
2024-08-30 11:21:22 +1000

    Fix comment format

--- settings.sh
+++ settings.sh
@@ -1,32 +1,31 @@
-# Settings for daily.sh
-###############################################################################
-# Limits for parallel work
+##  Settings for daily.sh
+
+#   Limits for parallel work
 MAX_SUBPROCESSES=16
 SIMULTANEOUS_TRANSFERS=32
 
-# Set a wait time in seconds
-# for any task that is attempted
-# repeatedly
+#   Set a wait time in seconds
+#   for any task that is attempted
+#   repeatedly
 WAIT=5.0
 
-# Number of attempts for checks
-# that repeat on fail
+#   Number of attempts for checks
+#   that repeat on fail
 ATTEMPTS=10
 
-# Make sure these units are active
+#   Make sure these units are active
 UNITS_TO_CHECK=( "syncthing@${USER}.service" \
                  'sshd.service' \
                  'apparmor.service' \
                  'updatedb.timer' )
 
-# Automatically ensure that these folders and files
-# are not copied to remote backups etc.
+#   Automatically ensure that these folders and files
+#   are not copied to remote backups etc.
 SECRET_FOLDERS=( '.ssh' '.gnupg' '.cert' '.pki' '.password-store' )
 SECRET_FILES=( "*.asc" "*.key" "*.pem" "id_rsa*" "id_dsa*" "id_ed25519*" )
 
-# Also keep these from being sent to cloud storage
+#   Also keep these from being sent to cloud storage
 SENSITIVE_FOLDERS=( '.git' '.stfolder' '.stversions' '.local' )
 
-# Symbols to separate output sections
+#   Symbols to separate output sections
 RULE="***"
-###############################################################################
