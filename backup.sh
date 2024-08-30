b7d3018182e622d5458a47b55a8389e7dba59056
Angus Gray-Weale <gusgw@gusgw.net>
2024-08-30 14:41:21 +1000

    Backup and archive routines in separate files

---
+++ backup.sh
@@ -0,0 +1,248 @@
+##  Run local and remote backups
+
+##  Settings
+#   STAMP               should be set by a call to set_stamp in useful.sh.
+#   KEY_FILE            decrypt local backup destination
+#   BACKUP_DISK         path to backup disk device by-uuid
+#   BACKUP_NAME         name to use in mount
+#   EXTRA_BACKUP_DISK   add a second backup disk by-uuid
+#   EXTRA_BACKUP_NAME   name of mount for second backup disk
+#   MAIN_WIRED          expected wired interface
+#   MAIN_WIRELESS       expected wireless interface
+#   SECRET_FOLDERS      make sure these folders are in .exclude_remote
+#   ~/.exclude_local    list of files and folders to exclude from local backup
+#   ~/.exclude_remote   list of files and folders to exclude from remote backup
+
+##  Default
+#   HOME
+
+##  Dependencies
+#   return_codes.sh
+#   settings.sh
+#   useful.sh
+
+##  Notes
+#   These routines copy to machines administered by me.
+#   See cloud.sh for archiving to S3 or the use of Proton
+#   and Google drives.
+#   Remote backups are run over MAIN_WIRED but not MAIN_WIRELESS.
+
+function run_local_backup {
+    # Local backup
+
+    ##########################################################################
+    # USES GLOBAL VARIABLES THAT SHOULD BE SET IN .bashrc OR .zshrc OR . . . #
+    ##########################################################################
+
+    >&2 echo "${STAMP}: run_local_backup"
+
+    local home_folder_name=$(basename $HOME)
+    local backup_destination="/mnt/${BACKUP_NAME}/${home_folder_name}"
+    local extra_backup_destination="/mnt/${EXTRA_BACKUP_NAME}/${home_folder_name}"
+
+    not_empty "date stamp" "$STAMP"
+    log_setting "name of the file with encryption key for local backup" \
+                "$KEY_FILE"
+    log_setting "device path for local encrypted backup" "$BACKUP_DISK"
+    log_setting "name of the backup" "$BACKUP_NAME"
+    log_setting "device path for extra local encrypted backup" "$EXTRA_BACKUP_DISK"
+    log_setting "name of extra backup" "$EXTRA_BACKUP_NAME"
+
+    check_exists "${HOME}/.exclude_local"
+
+    if [ -e "$BACKUP_DISK" ]; then
+        sudo cryptsetup open --key-file="$KEY_FILE"\
+                             "$BACKUP_DISK" \
+                             "$BACKUP_NAME" ||\
+            report $? "unlock backup disk"
+        sudo fsck -a "/dev/mapper/$BACKUP_NAME" ||\
+            report $? "running file system check on /dev/mapper/$BACKUP_NAME"
+        sudo mount "/dev/mapper/$BACKUP_NAME" "/mnt/$BACKUP_NAME" ||\
+            report $? "mount backup disk"
+        if [ -d "$backup_destination" ]; then
+            sudo rsync  -av \
+                        --links \
+                        --progress \
+                        --delete \
+                        --delete-excluded \
+                        --exclude-from="${HOME}/.exclude_local" \
+                        "${HOME}/" \
+                        "${backup_destination}/" ||\
+                    report $? "local backup via rsync"
+        else
+            >&2 echo "${STAMP}: local backup destination not found"
+        fi
+    else
+        >&2 echo "${STAMP}:  local backup device not found"
+    fi
+
+    if [ -e "$EXTRA_BACKUP_DISK" ]; then
+        sudo cryptsetup open --key-file="$KEY_FILE"\
+                             "$EXTRA_BACKUP_DISK" \
+                             "$EXTRA_BACKUP_NAME" ||\
+            report $? "unlock extra backup disk"
+        sudo fsck -a "/dev/mapper/$EXTRA_BACKUP_NAME" ||\
+            report $? "running file system check on /dev/mapper/$EXTRA_BACKUP_NAME"
+        sudo mount "/dev/mapper/$EXTRA_BACKUP_NAME" "/mnt/$EXTRA_BACKUP_NAME" ||\
+            report $? "mount extra backup disk"
+        if [ -d "$extra_backup_destination" ]; then
+            sudo rsync  -av \
+                        --links \
+                        --progress \
+                        --delete \
+                        --delete-excluded \
+                        --exclude-from="${HOME}/.exclude_local" \
+                        "${HOME}/" \
+                        "${extra_backup_destination}/" ||\
+                    report $? "extra local backup via rsync"
+        else
+            >&2 echo "${STAMP}: extra local backup destination not found"
+        fi
+    else
+        >&2 echo "${STAMP}: extra local backup device not found"
+    fi
+
+    return 0
+}
+
+function cleanup_local_backup {
+    # Clean up after local backup
+    # Make sure rsync is done
+
+    ######################################
+    # If using the report function here, #
+    # make sure it has NO THIRD ARGUMENT #
+    # or there will be an infinite loop! #
+    # This function may be used to       #
+    # handle trapped signals             #
+    ######################################
+
+    ##########################################################################
+    # USES GLOBAL VARIABLES THAT SHOULD BE SET IN .bashrc OR .zshrc OR . . . #
+    ##########################################################################
+
+    killall rsync || report $? "kill the rsync processes"
+    slow rsync
+
+    # If the local backup is mounted, unmount
+    if grep -qs "/mnt/${BACKUP_NAME}" /proc/mounts; then
+        sudo umount /dev/mapper/${BACKUP_NAME} ||\
+            report $? "unmounting backup"
+    fi
+
+    # Do not leave the encrypted backup unlocked
+    if sudo cryptsetup status ${BACKUP_NAME} 1> /dev/null; then
+        sudo cryptsetup close ${BACKUP_NAME} ||\
+            report $? "locking backup drive"
+    fi
+
+    # If the local backup is mounted, unmount
+    if grep -qs "/mnt/${EXTRA_BACKUP_NAME}" /proc/mounts; then
+        sudo umount /dev/mapper/${EXTRA_BACKUP_NAME} ||\
+            report $? "unmounting extra backup"
+    fi
+
+    # Do not leave the encrypted backup unlocked
+    if sudo cryptsetup status ${EXTRA_BACKUP_NAME} 1> /dev/null; then
+        sudo cryptsetup close ${EXTRA_BACKUP_NAME} ||\
+            report $? "locking extra backup drive"
+    fi
+
+    return 0
+}
+
+function run_remote_backup {
+    # Remote backup skipping sensitive data
+
+    ##########################################################################
+    # USES GLOBAL VARIABLES THAT SHOULD BE SET IN .bashrc OR .zshrc OR . . . #
+    ##########################################################################
+
+    >&2 echo "${STAMP}: run_remote_backup"
+
+    local src=$1
+    local dst=$2
+    local src_folder_name="$(basename $src)"
+    local backup_destination="${dst}/${src_folder_name}"
+
+    log_setting "directory to backup" "$src"
+    log_setting "address and path of remote backups" "$dst"
+    check_exists "${src}/.exclude_remote"
+
+    for f in ${SECRET_FOLDERS[@]}; do
+        if [ -d "${src}/$f" ]; then
+            check_contains "${src}/.exclude_remote" "$f"
+        fi
+    done
+
+    sudo rsync  -avz \
+                --links \
+                --progress \
+                --delete \
+                --delete-excluded \
+                --exclude-from="${src}/.exclude_remote" \
+                "${src}/" \
+                "${backup_destination}/" ||\
+            report "$?" "remote backup via rsync"
+
+    return 0
+}
+
+function cleanup_remote_backup {
+    # Clean up after remote backup
+    # Make sure rsync is done
+
+    ######################################
+    # If using the report function here, #
+    # make sure it has NO THIRD ARGUMENT #
+    # or there will be an infinite loop! #
+    # This function may be used to       #
+    # handle trapped signals             #
+    ######################################
+
+    killall rsync || report "$?" "kill the rsync processes"
+    slow rsync
+    return 0
+}
+
+function all_remote_backups {
+    # Run all desired remote backups to a
+    # a given destination. This does not
+    # include archive to object storage.
+
+    ##########################################################################
+    # USES GLOBAL VARIABLES THAT SHOULD BE SET IN .bashrc OR .zshrc OR . . . #
+    ##########################################################################
+
+    >&2 echo "${STAMP}: all_remote_backups"
+
+    local destination=$1
+
+    log_setting "destination for a $(hostnamectl hostname) backup set" \
+                "${destination}"
+
+    # Make sure we're excluding sensitive files
+    # from networked backups
+    for f in ${SECRET_FOLDERS[@]}; do
+        check_contains "${HOME}/.exclude_remote" "$f"
+    done
+
+    # Run remote backups over the wired connection only
+    if check_intfc "$MAIN_WIRED"; then
+        if ! check_intfc "$MAIN_WIRELESS"; then
+
+            run_remote_backup "${HOME}" "${destination}"
+
+            run_remote_backup '/mnt/data/archive' "${destination}"
+
+            if [ -n "${MONTH}" ]; then
+                if [ -d "/mnt/data/${MONTH}" ]; then
+                    run_remote_backup "/mnt/data/${MONTH}" "${destination}"
+                fi
+            fi
+
+            run_remote_backup '/mnt/data/wire' "${destination}"
+
+        fi
+    fi
+}
---
+++ cloud.sh
@@ -0,0 +1,185 @@
+##  Copy or archive to cloud storage services
+
+##  Settings
+#   STAMP                   should be set by a call to set_stamp in useful.sh.
+#   SHARED_STAGING          store a copy of shared folders ready for upload to a cloud drive
+#   SIMULTANEOUS_TRANSFERS  number of simultaneous transfers for rclone
+#   WAIT                    seconds between tries
+
+##  Dependencies
+#   return_codes.sh
+#   settings.sh
+#   useful.sh
+
+
+##  Notes
+#   rclone does not use the default configuration, instead it reads
+#   the more prominent ${HOME}/$(hostnamectl hostname)-rclone.conf.
+
+function run_archive {
+    # Run an encrypted archive to S3
+
+    ##########################################################
+    # The number of seconds to wait is globally set as $WAIT #
+    ##########################################################
+
+    ###################################################
+    # The number of transfers at once is globally set #
+    # as $SIMULTANEOUS_TRANSFERS                      #
+    ###################################################
+
+    >&2 echo "${STAMP}: run_archive"
+
+    local clear=$(realpath "$1")
+    local src=$(realpath "$2")
+    local remote=$3
+    local conf="${HOME}/$(hostnamectl hostname)-rclone.conf"
+
+    log_setting "cleartext to archive" "$clear"
+    log_setting "folder to archive" "$src"
+    log_setting "remote" "$remote"
+    log_setting "remote configuration" "$conf"
+
+    check_contains "$conf" "$remote"
+
+    if [ ! -d "${clear}" ]; then
+        >&2 echo "${STAMP}: cannot find ${clear}"
+        return "${MISSING_FOLDER}"
+    fi
+
+    if [ ! -d "${src}" ]; then
+        >&2 echo "${STAMP}: cannot find ${src}"
+        return "${MISSING_FOLDER}"
+    fi
+
+    if grep -qs "cryfs@${src} ${clear}" /proc/mounts; then
+        if remove_sensitive_data "${clear}"; then
+
+            listing=$(path_as_name ${src}) ||\
+                report $? "construct filename"
+            contents="${HOME}/${STAMP}-${listing}.txt"
+
+            echo "cryfs@${src} ${clear}" >> ${contents}
+            tree "${clear}" 1>> ${contents} ||\
+                report $? "save the tree of archived folders"
+
+            cryfs-unmount "${clear}" ||\
+                report $? "unmounting encrypted archive" \
+                          "no sync if archive is mounted"
+
+            while grep -qs "cryfs@${src} ${clear}" /proc/mounts; do
+                >&2 echo "${STAMP}: ${src} is mounted"
+                sleep "${WAIT}"
+            done
+            until [ -z "$(ls -A ${clear})" ]; do
+                >&2 echo "${STAMP}: ${clear} is not empty"
+                sleep "${WAIT}"
+            done
+
+            rclone sync --config  "$conf" \
+                        --progress \
+                        --transfers "${SIMULTANEOUS_TRANSFERS}" \
+                        --delete-excluded \
+                        --exclude cryfs.config \
+                        "${src}/" \
+                        "${remote}:" ||\
+                report $? "sync archive to remote"
+        else
+            >&2 echo "${STAMP}: failed to remove sensitive data"
+        fi
+    else
+        >&2 echo "${STAMP}: ${src} not mounted, cannot check security"
+        return "${MISSING_MOUNT}"
+    fi
+
+    return 0
+}
+
+function cleanup_run_archive {
+    # Clean up after remote backup
+    # Make sure rsync is done
+
+    ######################################
+    # If using the report function here, #
+    # make sure it has NO THIRD ARGUMENT #
+    # or there will be an infinite loop! #
+    # This function may be used to       #
+    # handle trapped signals             #
+    ######################################
+
+    killall rclone || report "$?" "kill the rclone processes"
+    slow rclone
+    return 0
+}
+
+function run_shared_preparation {
+    # Prepare some files for sharing via SHARED_STAGING
+
+    ##########################################################################
+    # USES GLOBAL VARIABLES THAT SHOULD BE SET IN .bashrc OR .zshrc OR . . . #
+    ##########################################################################
+
+    >&2 echo "${STAMP}: run_shared_preparation"
+
+    local src=$(realpath "$1")
+    local src_folder_name=$(basename $src)
+    local staging_area=$(realpath "${SHARED_STAGING}/${src_folder_name}")
+
+    log_setting "directory to backup" "$src"
+    log_setting "path to staging areas" "$staging_area"
+    check_exists "${src}/.include_shared"
+
+    # Clean out all folders from the staging area
+    for f in ${staging_area}/*; do
+        if [ -d "$f" ]; then
+            rm -rf $f
+        fi
+    done
+
+    # Synchroinuse to staging
+    while read f; do
+        echo $f
+        if [ -n "$f" ]; then
+            check_exists "${src}/${f}"
+            if [[ "$staging_area" != "${src}/${f}"* ]]; then
+                mkdir -p "${staging_area}/${f}"
+                rsync  -av \
+                       --links \
+                       --progress \
+                       --delete \
+                       "${src}/${f}/" \
+                       "${staging_area}/${f}/" ||\
+                    report "$?" "staging files via rsync"
+            else
+                >&2  echo "${STAMP}: $staging_area is in ${src}/${f}"
+                cleanup "$BAD_CONFIGURATION"
+            fi
+        fi
+    done < "${src}/.include_shared"
+
+    # Remove anything we do not share via cloud
+    remove_sensitive_data "${SHARED_STAGING}"
+
+    return 0
+}
+
+function cleanup_shared_preparation {
+    # Clean up after preparation of shared folders
+
+    ##########################################################################
+    # USES GLOBAL VARIABLES THAT SHOULD BE SET IN .bashrc OR .zshrc OR . . . #
+    ##########################################################################
+
+    killall rsync || report "$?" "kill the rsync processes"
+    slow rsync
+    remove_sensitive_data "${SHARED_STAGING}"
+    echo $(find "${SHARED_STAGING}" -type f | wc -l)  >\
+        "${SHARED_STAGING}/FILE_COUNT"
+    return 0
+}
+
+function handle_signal {
+    # cleanup and use error code if we trap a signal
+    >&2 echo "${STAMP}: trapped signal during maintenance"
+    cleanup "${TRAPPED_SIGNAL}"
+}
